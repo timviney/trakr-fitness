d3795efb8da3b3bf9cb2c6bf13bf87dcc6982360
timviney <t.c.viney@gmail.com>
2026-02-05 11:00:35 +0000

    Add CORS configuration to support allowed origins

--- src/server/GymTracker.Api/Program.cs
+++ src/server/GymTracker.Api/Program.cs
@@ -13,6 +13,29 @@ var runSwagger = builder.Configuration.GetValue<bool>("RunSwagger");
 
 if (runSwagger) SwaggerSetup.AddSwaggerServices(builder);
 
+var allowedOrigins = builder.Configuration
+    .GetSection("Cors:AllowedOrigins")
+    .Get<string[]>() ?? Array.Empty<string>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DefaultCors", policy =>
+    {
+        if (builder.Environment.IsDevelopment())
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        }
+        else if (allowedOrigins.Length > 0)
+        {
+            policy.WithOrigins(allowedOrigins)
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        }
+    });
+});
+
 var app = builder.Build();
 
 // Seed default data if enabled
@@ -21,6 +44,8 @@ await DbSetup.SeedDataAsync(app);
 if (!app.Environment.IsDevelopment()) app.UseHttpsRedirection();
 
 if (runSwagger) SwaggerSetup.UseSwagger(app);
+
+app.UseCors("DefaultCors");
 
 app.UseAuthentication();
 app.UseAuthorization();
